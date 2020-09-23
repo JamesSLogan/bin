6894f662d0c3722d53c19de438f0d61656f54310
James Logan <jamesstanleylogan@gmail.com>
2020-09-22 19:29:18 -0700

    initial files

---
+++ .vimrc
@@ -0,0 +1,13 @@
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+
+set noexpandtab
+set expandtab
+
+set ignorecase
+set smartcase
+
+set hlsearch
+set autoindent
+set noswapfile
---
+++ text.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import boto3
+
+if len(sys.argv) < 3 or sys.argv[1] == "-h" or sys.argv == "--help":
+    print("usage: text.py 18005555555 'message to be sent'\n")
+    sys.exit(0)
+
+number  = "+" + sys.argv[1]
+message = " ".join(sys.argv[2:])
+
+client = boto3.client("sns")
+response = client.publish (
+    PhoneNumber = number,
+    Message = message
+)
+print(response)
